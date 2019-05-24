2c5d2c8884d86d65475491ef266b7e2a7f085fdc
Nick Ramirez <nramirez@haproxy.com>
2019-05-24 12:06:54 -0400

    Changed least_session.lua to not use a table to be more efficient, per Bojan

--- fetch/least_sessions/least_sessions.lua
+++ fetch/least_sessions/least_sessions.lua
@@ -1,18 +1,14 @@
---
--- The contents of this file are Copyright (c) 2019. HAProxy Technologies. All Rights Reserved.
---
--- This file is subject to the terms and conditions defined in
--- file 'LICENSE', which is part of this source code package.
-
 local function backend_with_least_sessions(txn)
     -- Get the frontend that was used
     local fe_name = txn.f:fe_name()
 
-    local result = {}
+    local least_sessions_backend = ""
+    local least_sessions = 99999999999
 
     -- Loop through all the backends. You could change this
     -- so that the backend names are passed into the function too.
     for _, backend in pairs(core.backends) do
+
 
         -- Look at only backends that have names that start with
         -- the name of the frontend, e.g. "www_" prefix for "www" frontend.
@@ -28,24 +24,20 @@ local function backend_with_least_sessions(txn)
                 -- Get the backend's total number of current sessions
                 if stats['status'] == 'UP' then
                     total_sessions = total_sessions + stats['scur']
+                    core.Debug(backend.name .. ": " .. total_sessions)
                 end
             end
-
-            table.insert(result, {name = backend.name, total = total_sessions })
+          
+            if least_sessions > total_sessions then
+                least_sessions = total_sessions
+                least_sessions_backend = backend.name
+            end
         end
     end
 
-    -- Sort the backends by least number of sessions first
-    table.sort(result, function(a, b) return a['total'] < b['total'] end)
-
-    for k, v in pairs(result) do
-        core.Debug(k .. ": " .. v['name'] .. ", " .. v['total'])
-    end
-
     -- Return the name of the backend that has the fewest sessions
-    local k,v = next(result)
-    core.Debug("Returning: " .. v['name'])
-    return v['name']
+    core.Debug("Returning: " .. least_sessions_backend)
+    return least_sessions_backend
 end
 
 core.register_fetches('leastsess_backend', backend_with_least_sessions)
